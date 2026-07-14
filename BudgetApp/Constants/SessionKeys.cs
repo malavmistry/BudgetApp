b23e61f5c8c6d4b7d05211de5c105ec95695751c
MALAV MISTRY <malav345@gmail.com>
2026-07-13 23:37:22 -0400

    Enhance query to include additional linked budgets
    Revert "Enhance query to include additional linked budgets"
    
    This reverts commit 2b90defb7c8c0634926ea379f2d9dacf27120d91.

--- BudgetApp/Constants/SessionKeys.cs
+++
@@ -1,8 +0,0 @@
-namespace BudgetApp.Constants
-{
-    public static class SessionKeys
-    {
-        public const string LOGGED_IN_USER_ID = "LoggedInUserId";
-        public const string LOGGED_IN_USERNAME = "LoggedInUsername";
-    }
-}
--- BudgetApp/Enums/TransactionTypeEnums.cs
+++
@@ -1,8 +0,0 @@
-namespace BudgetApp.Enums
-{
-    public enum TransactionType
-    {
-        Expense = 1,
-        Earnings = 2
-    }
-}
